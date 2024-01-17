16b839844b79f5698b36cda98264f9c7ffbbd662
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-01-17 12:05:58 -0500

    feat(html): load page better

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 const rootRouter = require("./routes/root");
+
+// TODO: why does express.static("public"); not work, but this does?
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", rootRouter);
 
--- src/routes/root.js
+++ src/routes/root.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-    // https://stackoverflow.com/questions/25463423/res-sendfile-absolute-path
-    res.sendFile("public/index.html", { root: path.dirname(__dirname) });
+    res.render("index.html");
 });
 
 module.exports = router;
